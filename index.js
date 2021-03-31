184dba0d0311039c1ab0a4b673df88570c232b1e
M4TThys123 <matthijs.blauw2@gmail.com>
2021-03-31 14:56:20 +0200

    Eerste handelbars toegevoegd

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'))
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//homepagina
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home',{})
 });
 
+//movies
 app.get('/movies', (req, res) => {
     res.send('A List of movies')
-  });
+});
+
+//movies onderverdeelde pagina
+app.get('/movies/:movieId', (req, res) => { 
+    res.send(`<h1>Detailpagina van film ${req.params.movieId} </h1>`)
+});
+
+//404 foutmelding
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry we kunnen deze pagina niet vinden!")
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
