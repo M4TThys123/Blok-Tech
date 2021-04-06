587fc10186b1bc02b05e6ea2c0721ab0e4106124
M4TThys123 <matthijs.blauw2@gmail.com>
2021-04-06 12:24:52 +0200

    afbeeldingen

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const exphbs  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const movies = [
+  {title: "The Hulk", genre: "Action", jaartal: 1999},
+  {title: "Pulp Fiction", genre: "Action", jaartal: 1989},
+  {title: "Spidermen", genre: "Action", jaartal: 2012}
+]
+
 app.use(express.static('public'))
 
 app.engine('handlebars', exphbs());
@@ -15,7 +21,7 @@ app.get('/', (req, res) => {
 
 //movies
 app.get('/movies', (req, res) => {
-    res.send('A List of movies')
+    res.render('listOfMovies', {title: "lijst van films", movies})
 });
 
 //movies onderverdeelde pagina
@@ -32,3 +38,18 @@ app.use(function (req, res, next) {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
+
+/Deel Matthijs Featcher
+var chats = [
+  {"naam": "Matthijs", "afbeelding":"afb1", "games":"white"},
+  {"naam":"Kees", "afbeelding":"500", "games":"white"}
+
+ ]; 
+ 
+ app.post('/chat_home', async (req, res) => {
+  //  console.log(chats[0]["naam"]);
+   console.log(chats);
+  //  var chats =  new chats {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+  //  var newPerson = new Person("John", "Doe", "50", "blue");
+   });
